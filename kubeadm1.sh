bc200c3b27688efef8656560ea984144e706557e
Shipragit <shiva291291@gmail.com>
2025-01-02 19:05:43 +0530

    Update kubeadm1.sh

--- kubeadm1.sh
+++ kubeadm1.sh
@@ -23,8 +23,7 @@ sudo apt-get update
 sudo apt-get install -y software-properties-common curl
 
 # Add the Kubernetes repository
-sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key |
-    sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /" |
     sudo tee /etc/apt/sources.list.d/kubernetes.list
